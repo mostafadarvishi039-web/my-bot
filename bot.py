da26e0b6fbaa800180cd98acac0140d67a829905
mostafadarvishi039-web <mostafadarvishi039@gmail.com>
2026-08-24 02:30:41 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
-import os
+پimport os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-user_name = update.effective_user.first_name
-await update.message.reply_text(f"سلام {user_name} عزیز! ربات ابری شما روشن شد 🚀")
+    user_name = update.effective_user.first_name
+    await update.message.reply_text(f"سلام {user_name} عزیز! ربات ابری شما روشن شد 🚀")
 
-if name == 'main':
-TOKEN = os.environ.get("BOT_TOKEN")
-if not TOKEN:
-exit(1)
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+if __name__ == '__main__':
+    TOKEN = os.environ.get("BOT_TOKEN")
+    if not TOKEN:
+        exit(1)
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
